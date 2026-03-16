0048519ea58a746120dcb18883263fba123ca284
ave <ave@ave.zone>
2026-03-16 20:23:43 +0100

    disable crashlytics on debug builds

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -285,6 +285,9 @@ android {
         getByName("debug") {
             isMinifyEnabled = false
             isDebuggable = true
+            configure<com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension> {
+                mappingFileUploadEnabled = false
+            }
         }
     }
     compileOptions {
