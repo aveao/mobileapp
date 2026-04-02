ca753d0a78e545a6464ae1caba5704c7b3f49401
ave <git@ave.zone>
2026-04-02 18:17:32 +0200

    Add an app log viewer screen within settings

--- pebble/src/commonMain/kotlin/coredevices/pebble/ui/WatchSettingsScreen.kt
+++ pebble/src/commonMain/kotlin/coredevices/pebble/ui/WatchSettingsScreen.kt
@@ -1617,6 +1617,15 @@ fun rememberSettingsItemsState(navBarNav: NavBarNav?, snackbarDisplay: SnackbarD
                     },
                     show = { pebbleFeatures.supportsRestartingGattServerAfterBtPowerOn() }
                 ),
+                navBarNav?.let {basicSettingsActionItem(
+                    title = "View app logs",
+                    topLevelType = TopLevelType.Phone,
+                    section = Section.Diagnostics,
+                    action = {
+                        navBarNav.navigateTo(CommonRoutes.LogViewerRoute)
+                    },
+                    show = { debugOptionsEnabled },
+                ) },
                 basicSettingsActionItem(
                     title = "Post test notification",
                     description = "Create a test notification, with actions",
